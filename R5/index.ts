eae10035b4c32c4d498e75390fb1e8f7e6f696f8
ydfk <lyh6728326@gmail.com>
2021-03-11 17:34:12 +0800

    完善暴力算法

--- R5/index.ts
+++ R5/index.ts
@@ -3,9 +3,33 @@
  * @Author: ydfk
  * @Date: 2021-03-10 14:36:27
  * @LastEditors: ydfk
- * @LastEditTime: 2021-03-10 14:36:28
+ * @LastEditTime: 2021-03-11 17:26:21
  */
 
 import r1 from "./rboot1";
+import r from "./rboot";
 
-console.log(r1("URR",[],3,2))
+// const x = 33;
+// const y = 55;
+// const command = "URRURRRRRUUUUURRURUURURURUURURUURURUUUUURRRRRUURUURUUUUURRRRRUUUUURUUUURUUUUURUUUUURUUUUU";
+// const obstacles: number[][] = [
+//   [12, 22],
+//   [44, 88],
+// ];
+
+const x = 75412346;
+const y = 45764134;
+const command = "URR";
+const obstacles: number[][] = [];
+
+console.log("终点 x,y", x, y);
+console.log("命令 command", command);
+console.log("障碍物 obstacles", obstacles);
+
+console.time("花费时间");
+console.log("优化算法运行结果：", r1(command, obstacles, x, y));
+console.timeEnd("优化算法花费时间");
+
+console.time("暴力算法花费时间");
+console.log("暴力算法运行结果：", r(command, obstacles, x, y));
+console.timeEnd("暴力算法花费时间");
--- R5/rboot1.ts
+++ R5/rboot1.ts
@@ -3,32 +3,50 @@
  * @Author: ydfk
  * @Date: 2021-03-10 14:45:47
  * @LastEditors: ydfk
- * @LastEditTime: 2021-03-10 15:34:46
+ * @LastEditTime: 2021-03-11 10:46:47
  */
-
-//暴力算法
+//通过第一轮的路径上的落点，推算全部路径上落点的规律
 export default (command: string, obstacles: number[][], x: number, y: number): boolean => {
-  return isPass(command, x, y, 0,0,0) > 0
+  // 记录第一轮走过的所有点
+  const paths: number[][] = [];
+
+  let xStep = 0;
+  let yStep = 0;
+
+  for (let i = 0; i < command.length; i++) {
+    if (command[i] === "R") {
+      xStep += 1;
+    } else {
+      yStep += 1;
+    }
+
+    paths[i] = [xStep, yStep];
+  }
+
+  //判断是否会撞墙
+  if (obstacles != []) {
+    for (let obstacle of obstacles) {
+      if (obstacle[0] >= x && obstacle[1] >= y) {
+        continue;
+      }
+
+      if (checkPoint(paths, obstacle[0], obstacle[1], xStep, yStep)) {
+        return false;
+      }
+    }
+  }
+
+  // 判断是否会到达终点
+  return checkPoint(paths, x, y, xStep, yStep);
 };
 
-function isPass(command: string, x: number, y: number, dx: number, dy: number, count: number): number {
-  for (let c of command) {
-    if (c === "R") {
-      dx++;
-    } else {
-      dy++;
-    }
-
-    count++;
-
-    if (dx == x && dy == y) {
-      return count;
+function checkPoint(paths: number[][], x: number, y: number, xStep: number, yStep: number): boolean {
+  // (x-x1)%xStep=0 (y-y1)%yStep = 0
+  for (let path of paths) {
+    if ((x - path[0]) % xStep == 0 && (y - path[1]) % yStep == 0 && (x - path[0]) / xStep == (y - path[1]) / yStep) {
+      return true;
     }
   }
 
-  if (dx > x || dy > y) {
-    return -1;
-  } else {
-    return isPass(command, x, y, dx, dy, count);
-  }
+  return false;
 }
